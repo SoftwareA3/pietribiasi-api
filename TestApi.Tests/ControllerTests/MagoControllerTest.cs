0ca17a9faa1a80b9860998cd3aa2dc69aeebd24f
RaulPianon <raul.pianon@studenti.unipd.it>
2025-06-20 12:54:37 +0200

    Aggiunti test

--- TestApi.Tests/ControllerTests/MagoControllerTest.cs
+++ TestApi.Tests/ControllerTests/MagoControllerTest.cs
@@ -51,6 +51,20 @@ namespace TestApi.Tests.ControllerTests
             WorkerId = 1
         };
 
+        private readonly SettingsDto _testSettingsDto = new SettingsDto
+        {
+            MagoUrl = "http://example.com",
+            Username = "testUser",
+            Password = "testPassword",
+            Company = "TestCompany",
+            SpecificatorType = 1,
+            Closed = false,
+            RectificationReasonPositive = "Positive Reason",
+            RectificationReasonNegative = "Negative Reason",
+            Storage = "TestStorage",
+            SyncGlobalActive = true
+        };
+
         private readonly SyncRegOreRequestDto _testSyncRegOreRequest = new SyncRegOreRequestDto
         {
             Closed = false,
@@ -103,6 +117,42 @@ namespace TestApi.Tests.ControllerTests
             }
         };
 
+        private readonly SyncRegOreFilteredDto _testSyncRegOreFilteredDto = new SyncRegOreFilteredDto
+        {
+            WorkerIdSyncRequestDto = new WorkerIdSyncRequestDto { WorkerId = 1 },
+            RegOreList = new List<RegOreDto>
+            {
+                new RegOreDto
+                {
+                    RegOreId = 1,
+                    WorkerId = 43,
+                    SavedDate = new DateTime(2023, 10, 1, 12, 0, 0),
+                    Job = "JOB123",
+                    RtgStep = 1,
+                    Alternate = "ALT1",
+                    AltRtgStep = 2,
+                    Operation = "OP123",
+                    OperDesc = "Operation Description",
+                    Bom = "BOM123",
+                    Variant = "VAR1",
+                    ItemDesc = "Item Description",
+                    Moid = 100,
+                    Mono = "MO123",
+                    CreationDate = new DateTime(2023, 9, 1),
+                    Uom = "HR",
+                    ProductionQty = 100,
+                    ProducedQty = 75,
+                    ResQty = 25,
+                    Storage = "WH01",
+                    Wc = "WC01",
+                    WorkingTime = 480,
+                    Imported = false,
+                    UserImp = "User123",
+                    DataImp = new DateTime(2023, 10, 1, 12, 0, 0),
+                }
+            }
+        };
+
         private readonly SyncInventarioRequestDto _testSyncInventarioRequest = new SyncInventarioRequestDto
         {
             MA_InventoryEntries = new MA_InventoryEntries
@@ -126,6 +176,8 @@ namespace TestApi.Tests.ControllerTests
             }
         };
 
+        private readonly (MagoLoginResponseDto, SettingsDto) _loginResponseTuple;
+
         private readonly SyncronizedDataDto _testSyncronizedData;
 
         public MagoControllerTests()
@@ -142,6 +194,13 @@ namespace TestApi.Tests.ControllerTests
             {
                 HttpContext = httpContextMock.Object
             };
+
+            _loginResponseTuple = (_testMagoLoginResponse, _testSettingsDto);
+
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest))
+                .Returns(Task.FromResult(_loginResponseTuple));
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(It.Is<MagoTokenRequestDto>(dto => dto.Token == _testMagoLoginResponse.Token)))
+                .Returns(Task.CompletedTask);
 
             _responseHandlerMock.Setup(rh => rh.HandleNoContent(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
                 .Returns(new NoContentResult());
@@ -183,18 +242,235 @@ namespace TestApi.Tests.ControllerTests
         {
             // Arrange
             _magoRequestServiceMock.Setup(s => s.LoginAsync(_testMagoLoginRequest))
-                .ReturnsAsync(_testMagoLoginResponseWithError);
+                .ReturnsAsync((MagoLoginResponseDto)null);
+            _responseHandlerMock.Setup(rh => rh.HandleNotFound(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new NotFoundObjectResult("Not Found"));
+
+            // Act
+            var result = await _controller.Login(_testMagoLoginRequest);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("Not Found", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task Login_ShouldReturnBadRequest_WhenRequestIsInvalid()
+        {
+            // Arrange
+            _magoRequestServiceMock.Setup(s => s.LoginAsync(It.IsAny<MagoLoginRequestDto>()))
+                .Throws(new ArgumentException("Invalid request"));
             _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
                 .Returns(new BadRequestObjectResult("Bad Request"));
 
             // Act
-            var result = await _controller.Login(_testMagoLoginRequest);
+            var result = await _controller.Login(null);
 
             // Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("Bad Request", badRequestResult.Value);
         }
 
-        
+        // -- Test per Logoff --
+        [Fact]
+        public async Task Logoff_ShouldReturnOk_WhenLogoffIsSuccessful()
+        {
+            // Arrange
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(_testMagoTokenRequest));
+            _responseHandlerMock.Setup(rh => rh.HandleOk(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new OkObjectResult(""));
+
+            // Act
+            var result = await _controller.LogOff(_testMagoTokenRequest);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            _magoRequestServiceMock.Verify(s => s.LogoffAsync(_testMagoTokenRequest), Times.Once);
+        }
+
+        [Fact]
+        public async Task Logoff_ShouldReturnBadRequest_WhenLogoffFails()
+        {
+            // Arrange
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(It.IsAny<MagoTokenRequestDto>()));
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.LogOff(null);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Logoff_ShouldReturnBadRequest_WhenServiceThrowsException()
+        {
+            // Arrange
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(It.IsAny<MagoTokenRequestDto>()))
+                .Throws(new Exception("Logoff failed"));
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.LogOff(_testMagoTokenRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Syncronize_ShouldReturnOk_WhenSyncronizationIsSuccessful()
+        {
+            // Arrange
+            var loginResponseTuple = (_testMagoLoginResponse, _testSettingsDto);
+
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest))
+                .Returns(Task.FromResult(loginResponseTuple));
+
+            _magoRequestServiceMock.Setup(s => s.SyncronizeAsync(_testMagoLoginResponse, _testSettingsDto, _testWorkerIdSyncRequest))
+                .ReturnsAsync(_testSyncronizedData);
+
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(It.Is<MagoTokenRequestDto>(dto => dto.Token == _testMagoLoginResponse.Token)))
+                .Returns(Task.CompletedTask);
+
+            _responseHandlerMock.Setup(rh => rh.HandleOkAndItem(It.IsAny<HttpContext>(), _testSyncronizedData, It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new OkObjectResult("Syncronization successful"));
+
+            // Act
+            var result = await _controller.Syncronize(_testWorkerIdSyncRequest);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Syncronization successful", okResult.Value);
+
+            // Verify that all expected methods were called
+            _magoRequestServiceMock.Verify(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest), Times.Once);
+            _magoRequestServiceMock.Verify(s => s.SyncronizeAsync(_testMagoLoginResponse, _testSettingsDto, _testWorkerIdSyncRequest), Times.Once);
+            _magoRequestServiceMock.Verify(s => s.LogoffAsync(It.Is<MagoTokenRequestDto>(dto => dto.Token == _testMagoLoginResponse.Token)), Times.Once);
+        }
+
+        [Fact]
+        public async Task Syncronize_ShouldReturnBadRequest_WhenReqestIsInvalid()
+        {
+            // Arrange
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(It.IsAny<WorkerIdSyncRequestDto>()))
+                .Throws(new ArgumentException("Invalid request"));
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.Syncronize(null);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Syncronize_ShouldReturnBadRequest_WhenLoginFails()
+        {
+            // Arrange
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.Syncronize(null);
+
+            // Assert
+            var notFoundResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task Syncronize_ShouldReturnBadRequest_WhenServiceThrowsException()
+        {
+            // Arrange
+            var loginResponseTuple = (_testMagoLoginResponse, _testSettingsDto);
+
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest))
+                .ReturnsAsync(loginResponseTuple);
+
+            _magoRequestServiceMock.Setup(s => s.SyncronizeAsync(_testMagoLoginResponse, _testSettingsDto, _testWorkerIdSyncRequest))
+                .Throws(new Exception("Syncronization failed"));
+
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(It.Is<MagoTokenRequestDto>(dto => dto.Token == _testMagoLoginResponse.Token)))
+                .Returns(Task.CompletedTask);
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.Syncronize(_testWorkerIdSyncRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Syncronize_ShouldReturBadRequest_WhenLoginResponseIsNull()
+        {
+            // Arrange
+            (MagoLoginResponseDto, SettingsDto) loginResponseTuple = (null, _testSettingsDto);
+
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest))
+                .Returns(Task.FromResult(loginResponseTuple));
+
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.Syncronize(_testWorkerIdSyncRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Syncronize_ShouldReturBadRequest_WhenSettingsIsNull()
+        {
+            // Arrange
+            (MagoLoginResponseDto, SettingsDto) loginResponseTuple = (_testMagoLoginResponse, null);
+
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest))
+                .Returns(Task.FromResult(loginResponseTuple));
+
+            _responseHandlerMock.Setup(rh => rh.HandleBadRequest(It.IsAny<HttpContext>(), It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new BadRequestObjectResult("Bad Request"));
+
+            // Act
+            var result = await _controller.Syncronize(_testWorkerIdSyncRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Bad Request", badRequestResult.Value);
+        }
+
+        // -- Test per SyncRegOreFiltered --
+        [Fact]
+        public async Task SyncRegOreFiltered_ShouldReturnOk_WhenSyncIsSuccessful()
+        {
+            // Arrange
+            var loginResponseTuple = (_testMagoLoginResponse, _testSettingsDto);
+
+            _magoRequestServiceMock.Setup(s => s.LoginWithWorkerIdAsync(_testWorkerIdSyncRequest))
+                .Returns(Task.FromResult(loginResponseTuple));
+            _magoRequestServiceMock.Setup(s => s.LogoffAsync(It.Is<MagoTokenRequestDto>(dto => dto.Token == _testMagoLoginResponse.Token)))
+                .Returns(Task.CompletedTask);
+            _magoRequestServiceMock.Setup(s => s.SyncRegOreFiltered(_testMagoLoginResponse, _testSettingsDto, _testSyncRegOreFilteredDto, true))
+                .Returns(Task.FromResult<IEnumerable<SyncRegOreRequestDto>>(new List<SyncRegOreRequestDto> { _testSyncRegOreRequest }));
+            _responseHandlerMock.Setup(rh => rh.HandleOkAndItem(It.IsAny<HttpContext>(), It.IsAny<SyncRegOreRequestDto>, It.IsAny<bool>(), It.IsAny<string>()))
+                .Returns(new OkObjectResult("Sync successful"));
+
+            // Act
+            var result = await _controller.SyncRegOreFiltered(_testSyncRegOreFilteredDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Sync successful", okResult.Value);
+        }
     }
 }
